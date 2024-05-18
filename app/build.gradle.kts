c3d9f1b7ed0a5048476d050e97219330220db64a
elib00 <joshuanapinas19@gmail.com>
2024-05-18 22:20:20 +0800

    [Joshua] Added maps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.android.libraries.mapsplatform.secrets.gradle.plugin)
 }
 
 android {
@@ -31,12 +32,18 @@ android {
     }
 }
 
-dependencies {
+secrets {
+    defaultPropertiesFileName = "secrets.properties"
+}
 
+dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.play.services.maps)
+    implementation(libs.play.services.location)
+    implementation(libs.dexter)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
--- app/src/main/java/com/example/roomradar/MainActivity.java
+++ app/src/main/java/com/example/roomradar/MainActivity.java
@@ -1,24 +1,55 @@
 package com.example.roomradar;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final HomeFragment homeFragment = new HomeFragment();
+    private final NotificationsFragment notificationsFragment = new NotificationsFragment();
+    private final MapFragment mapFragment = new MapFragment();
+    private final ProfileFragment profileFragment = new ProfileFragment();
+    public BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
+        initializeView();
+    }
+
+    private void initializeView(){
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavContainer);
+
+        bottomNavigationView.setSelectedItemId(R.id.homeItem);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentsContainer, homeFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId() == R.id.homeItem){
+                    System.out.println("home ni");
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentsContainer, homeFragment).addToBackStack(null).commit();
+                }else if(item.getItemId() == R.id.mapsItem){
+                    System.out.println("maps ni");
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentsContainer, mapFragment).addToBackStack(null).commit();
+                }else if(item.getItemId() == R.id.notificationsItem){
+                    System.out.println("notifs ni");
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentsContainer, notificationsFragment).addToBackStack(null).commit();
+                }else if(item.getItemId() == R.id.profileItem){
+                    System.out.println("profile ni");
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentsContainer, profileFragment).addToBackStack(null).commit();
+                }
+
+                return true;
+            }
+        });
     }
 }
